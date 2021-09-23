cd96cd2aee5b10615a6fe2278d9ea4ebaab816a8
Luis Carlos <luis.carlos@jornadatecnologia.com>
2021-09-23 20:54:32 -0300

    [features] Instalando tema material-ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { useEffect } from "react";
 import useOnlineStatus from "./data/hooks/useOnlineStatus";
+import { Button } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./ui/themes/theme";
 
 function App() {
   const isOnline = useOnlineStatus();
 
   useEffect(() => {
-    if(!isOnline){
+    if (!isOnline) {
       alert("Sua conexão caiu!");
     }
-  }, [isOnline])
+  }, [isOnline]);
 
-  return <div>{isOnline ? "Você esta Online" : "Você esta desconectado"}</div>;
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <div>
+          {isOnline ? "Você esta Online" : "Você esta desconectado"}
+          <Button variant={"contained"}>click</Button>
+        </div>
+      </ThemeProvider>
+      <Button variant={"contained"}>click</Button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/ui/themes/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff808",
+    },
+  },
+});
+
+export default theme;
